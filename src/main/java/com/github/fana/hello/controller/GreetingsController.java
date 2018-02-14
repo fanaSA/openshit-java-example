d793974b693d654cc43a4041b7b06bac2dfc33f6
fsithole <fsithole1@gmail.com>
2018-02-14 13:40:32 +0200

    Basic hello endpoint

---
+++ src/main/java/com/github/fana/hello/controller/GreetingsController.java
@@ -0,0 +1,26 @@
+package com.github.fana.hello.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/greet", produces = MediaType.APPLICATION_JSON_VALUE)
+public class GreetingsController {
+
+    @GetMapping
+    public String greetStranger(){
+        return greetWithName("Stranger");
+    }
+
+    @GetMapping("/{name}")
+    public String greetKnownPerson(@PathVariable("name") String name){
+        return greetWithName(name);
+    }
+
+    private String greetWithName(String name){
+        return "Hello, " + name + "!";
+    }
+}
